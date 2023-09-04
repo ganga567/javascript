802681093dcab6834de46e662418174ffd7bc6df
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2023-09-04 11:02:53 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,106 @@
+//varibles
+//var
+var bala =25;
+//let
+let a=2+3;
+//const
+const  java = "welcome to java script";
+//---------------------------------------------------------------------
+//Datatypes----
+// integer
+let num = 20;
+
+// float
+let floatNum = 3.33;
+
+// string
+ let fruit = "apple";
+
+// object
+let person = {
+    name : "Gangadhar",
+    age: 24
+}
+//array
+ let Numb = [10,20,30];
+
+// null
+let nullVariable = null;
+
+//bollean
+let isTrue = true;
+
+let isFalse = false;
+
+//undefined
+let Gangadhar
+//----------------------------------------------------------------------------
+
+
+//printing varibles
+console.log(bala)
+console.log(a)
+console.log(java)
+
+// arthematic operators--
+let addition = 5+6;
+let subraction = 15-6;
+let multiplication= 5*6;
+let division= 5/6;
+let modulue = 5%6;
+//----------------------------------------------------------
+//logical operators-
+// AND (&&)--
+
+let isBala = true; 
+let isGangadhar = false;
+// OR (||):
+let isSava = true;
+let isScript = false;
+
+// NOT (!):
+var isGo = true;
+var isStop = false;
+
+//-----------------------------------------------------------
+
+//resional operators--
+let isEqual = 5===5;
+let isNotEqual = 20 !==5;
+let isGreaterThan = 8>5;
+let isGreaterOrEqual= 3>=6;
+let isLessThan= 5<5;
+let isLessOrEqual = 5<=5;
+
+
+//printing arthematic operators---------------------------
+console.log(addition)
+console.log(subraction)
+console.log(multiplication)
+console.log(division)
+console.log(modulue)
+
+//printing logical operators-------------------------------
+console.log(isTrue && isFalse);
+console.log(isTrue || isFalse);
+
+console.log(!isTrue);
+console.log(!isFalse);   
+
+
+//printing resional operators-------------------------------
+console.log(isEqual)
+console.log(isNotEqual)
+console.log(isGreaterThan)
+console.log(isLessOrEqual)
+console.log(isLessThan)
+console.log(isGreaterOrEqual)
+
+//printing Datatypes----------------------------------------
+console.log(num)
+console.log(floatNum)
+console.log(fruit)
+console.log(person)
+console.log(nullVariable)
+console.log(Numb)
+console.log(isTrue,isFalse)
