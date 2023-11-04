be2f56d2fa81fc9f7349fa1059ec31a694c2954a
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2023-11-04 10:02:39 +0530

    Add files via upload

---
+++ positiveandnegative.js
@@ -0,0 +1,30 @@
+// program that checks if the number is positive, negative or zero
+// input from the user
+const number1 = parseInt(prompt("Enter a number: "));
+
+// check if number is greater than 0
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+// check if number is 0
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+// if number is less than 0
+else {
+     console.log("The number is negative");
+}
+// check if the number is positive, negative or zero
+const number = prompt("Enter a number: ");
+
+if (number2 >= 0) {
+    if (number == 0) {
+        console.log("The number is zero");
+    } else {
+        console.log("The number is positive");
+    }
+} else {
+    console.log("The number is negative");
+}                                                                                                       
