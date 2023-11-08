6f497d621bb0e1abe20b7c048409f6ea32c40470
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2023-11-09 01:14:39 +0530

    Add files via upload

---
+++ displaydateandtime.js
@@ -0,0 +1,14 @@
+// program to display the date and time
+// get date and time
+const date = new Date(2017, 2, 12, 9, 25, 30);
+
+
+const n = date.toDateString();
+
+
+const time = date.toLocaleTimeString();
+
+console.log('Date: ' + n);
+
+// display time
+console.log('Time: ' + time);
