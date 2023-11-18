3aa2e09c271a4efcd3331ed4b61095175e1332d7
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2023-11-18 15:13:25 +0530

    Add files via upload

---
+++ iterating.js
@@ -0,0 +1,11 @@
+student = {
+    "name": "Sagar",
+    "age": 20,
+    "grade": "A"
+}
+print("Name:", student["name"])
+print("Age:", student["age"])
+
+# Iterating through dictionary keys and values
+for key, value in student.items():
+    print(key, ":", value)
